3916f6d9db583e04c0946c5ffe8590b5cf32832e
Jakewilly14 <jakewilly14@gmail.com>
2023-10-20 17:51:39 -0500

    Fixed bug, chagned styling, added class and ID

--- assets/script.js
+++ assets/script.js
@@ -7,47 +7,48 @@ $('#current-date').text(currentDate);
 // We will check dayjs values for expired hours of the day
 var nineAm = $('#nine-am');
 nineAm = dayjs().hour(9).hour();
-console.log(nineAm);
+//console.log(nineAm);
 
 var tenAm = $('#ten-am');
 tenAm = dayjs().hour(10).hour();
-console.log(tenAm);
+//console.log(tenAm);
 
 var elevenAm = $('#eleven-am');
 elevenAm = dayjs().hour(11).hour();
-console.log(elevenAm);
+//console.log(elevenAm);
 
 var twelvePm = $('#twelve-pm');
 twelvePm = dayjs().hour(12).hour();
-console.log(twelvePm);
+//console.log(twelvePm);
 
 var onePm = $('#one-pm');
 onePm = dayjs().hour(13).hour();
-console.log(onePm);
+//console.log(onePm);
 
 var twoPm = $('#two-pm');
 twoPm = dayjs().hour(14).hour();
-console.log(twoPm);
+//console.log(twoPm);
 
 var threePm = $('#three-pm');
 threePm = dayjs().hour(15).hour();
-console.log(threePm);
+//console.log(threePm);
 
 var fourPm = $('#four-pm');
 fourPm = dayjs().hour(16).hour();
-console.log(fourPm);
+//console.log(fourPm);
 
 var fivePm =$('#five-pm');
 fivePm = dayjs().hour(17).hour();
-console.log(fivePm);
+//console.log(fivePm);
 
-
+// Creating variable for just the hour of the day
 var currentHour = dayjs().hour();
 console.log(currentHour);
+
 // Nine Am if statement
 if (dayjs() >= nineAm && dayjs() < tenAm) {
   $('#nine-text').addClass('current');
-} else if (dayjs() >= 1 && dayjs() < dayjs().hour(8).minute(59)){
+} else if (dayjs() >= 1 && dayjs() < dayjs().hour(7).minute(59)){
   $('#nine-text').addClass('upcoming');
 } else {
   $('#nine-text').addClass('expired');
@@ -63,7 +64,7 @@ if (dayjs() >= tenAm && dayjs() < elevenAm) {
 // Eleven Am if statement
 if (dayjs() >= elevenAm && dayjs() < twelvePm) {
   $('#eleven-text').addClass('current');
-} else if (dayjs() >= 1 && dayjs() < dayjs().hour(8).minute(59)){
+} else if (dayjs() >= 1 && dayjs() < dayjs().hour(9).minute(59)){
   $('#eleven-text').addClass('upcoming');
 } else {
   $('#eleven-text').addClass('expired');
@@ -71,7 +72,7 @@ if (dayjs() >= elevenAm && dayjs() < twelvePm) {
 // Twelve Pm if statement
 if (dayjs() >= twelvePm && dayjs() < onePm) {
   $('#twelve-text').addClass('current');
-} else if (dayjs() >= 1 && dayjs() < dayjs().hour(8).minute(59)){
+} else if (dayjs() >= 1 && dayjs() < dayjs().hour(11).minute(59)){
   $('#twelve-text').addClass('upcoming');
 } else {
   $('#twelve-text').addClass('expired');
@@ -79,7 +80,7 @@ if (dayjs() >= twelvePm && dayjs() < onePm) {
 // One Pm if statement
 if (dayjs() >= onePm && dayjs() < twoPm) {
   $('#one-text').addClass('current');
-} else if (dayjs() >= 1 && dayjs() < dayjs().hour(8).minute(59)){
+} else if (dayjs() >= 1 && dayjs() < dayjs().hour(12).minute(59)){
   $('#one-text').addClass('upcoming');
 } else {
   $('#one-text').addClass('expired');
@@ -87,7 +88,7 @@ if (dayjs() >= onePm && dayjs() < twoPm) {
 // Two Pm if statement
 if (dayjs() >= twoPm && dayjs() < threePm) {
   $('#two-text').addClass('current');
-} else if (dayjs() >= 1 && dayjs() < dayjs().hour(8).minute(59)){
+} else if (dayjs() >= 1 && dayjs() < dayjs().hour(13).minute(59)){
   $('#two-text').addClass('upcoming');
 } else {
   $('#two-text').addClass('expired');
@@ -95,7 +96,7 @@ if (dayjs() >= twoPm && dayjs() < threePm) {
 // Three Pm if statement
 if (dayjs() >= threePm && dayjs() < fourPm) {
   $('#three-text').addClass('current');
-} else if (dayjs() >= 1 && dayjs() < dayjs().hour(8).minute(59)){
+} else if (dayjs() >= 1 && dayjs() < dayjs().hour(14).minute(59)){
   $('#three-text').addClass('upcoming');
 } else {
   $('#three-text').addClass('expired');
@@ -103,16 +104,20 @@ if (dayjs() >= threePm && dayjs() < fourPm) {
 // Four Pm if statement
 if (dayjs() >= fourPm && dayjs() < fivePm) {
   $('#four-text').addClass('current');
-} else if (dayjs() >= 1 && dayjs() < dayjs().hour(8).minute(59)){
+} else if (dayjs() >= 1 && dayjs() < dayjs().hour(15).minute(59)){
   $('#four-text').addClass('upcoming');
 } else {
   $('#four-text').addClass('expired');
 }
 // Five Pm if statement
-if (dayjs() >= fivePm && dayjs() < dayjs().hour(6)) {
+if (dayjs() > fivePm && dayjs() < dayjs().hour(18)) {
   $('#five-text').addClass('current');
-} else if (dayjs() >= 1 && dayjs() < dayjs().hour(8).minute(59)){
+} else if (dayjs() >= 1 && dayjs() < dayjs().hour(16).minute(59)){
   $('#five-text').addClass('upcoming');
 } else {
   $('#five-text').addClass('expired');
 }
+console.log(dayjs().hour());
+
+// Add event listeners for Save buttons
+// Write function to save written message to local storage 
